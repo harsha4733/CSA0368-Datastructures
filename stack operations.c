9f2d1d5dbf074860a29ee32f3efc396401e30280
Harshavardhan Guggilla <113342518+harsha4733@users.noreply.github.com>
2022-09-14 13:49:50 +0530

    Create stack operations.c

---
+++ stack operations.c
@@ -0,0 +1,83 @@
+# CSA0368-Datastructures
+programs on data structures lab
+#include<stdio.h>
+#include<stdlib.h>
+#include<process.h>
+define SIZE 5
+
+int top=-1;
+int stk[SIZE];
+int push()
+{
+    int ele;
+    printf("enter the element");
+    scanf("%d",&ele);
+    if(top==SIZE-1)
+        printf("the stack is full");
+    else
+    {
+
+        top=top+1;
+        stk[top]=ele;
+    }
+}
+int pop()
+{   
+    int ele;4444533
+    if(top==-1)
+        printf("the stack is empty");
+    else
+     {
+
+        ele=stk[top];
+        top=top-1;
+     }
+    printf("the deleted element %d",ele);
+}
+int peak()
+{
+    if(top==-1)
+        printf("the stack is empty");
+    else
+        printf("the peak value in the stack %d",stk[top]);
+}
+int display()
+{
+    int i;
+    if(top==-1)
+        printf("stack is empty");
+    else
+        for(i=top;i>=0;i--)
+        {
+            printf("%d  ",stk[i]);
+        }
+}
+int main()
+{
+    int ch;
+    do
+    {
+    printf("\n1.push\n2.pop\n3.peak\n4.display\n5.exit");
+    printf("enter the choice");
+    scanf("%d",&ch);
+    switch(ch)
+    {
+        case 1:
+            push();
+            break;
+        case 2:
+            pop();
+            break;
+        case 3:
+            peak();
+            break;
+        case 4:
+            display();
+            break;
+        case 5:
+            exit(0);
+        default:
+            printf("\n wrong input");
+    }
+    } while(ch>=0 && ch<=5);
+}
